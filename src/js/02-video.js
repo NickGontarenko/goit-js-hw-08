2a2a1038c0fda59cfba02f50be2e404952d9e50b
NickGontarenko <nick.gontarenko@gmail.com>
2022-02-06 13:34:49 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,13 @@
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const vimeoPlayerRef = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(vimeoPlayerRef);
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-  }, 1000),
-);
+const entryInLocalStorage = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(entryInLocalStorage, 1000));
 
 const currentTime = localStorage.getItem('videoplayer-current-time');
 
